857a263c710a27f8fd4a14daa4f1d82cd4a544e2
danndnts <164400557+danndnts@users.noreply.github.com>
2024-06-05 15:41:33 -0300

    Add files via upload

--- boletim_de_notas.py
+++ boletim_de_notas.py
@@ -1,1 +1,5 @@
 import tkinter as tk
+
+def criar_arquivo():#Funcao para criar os arquivos.
+    with open("dados.txt", "w") as arquivo:#o modo "w" que se não e existir ele cria e se já houver uma com o mesmo nome ele remove
+        arquivo.write("")#Uma string vazia dentro do arquivo
